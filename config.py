4f171fc5e008572975e266aeb2b696f2da45894a
alfahrifitacademy <alfahri.fitacademy@gmail.com>
2024-11-15 12:58:35 +0700

    Update

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
 
     # Konfigurasi database (gunakan nama db_movie)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://dbmovie_condition:3e8298c113c4e068502e9b52853c33f80e3b9dea@e6-t9.h.filess.io:3307/dbmovie_condition'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://dbmovie_condition:3e8298c113c4e068502e9b52853c33f80e3b9dea@e6-t9.h.filess.io:3307/dbmovie_condition'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Inisialisasi SQLAlchemy dengan aplikasi
